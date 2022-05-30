0ec7310c4e0eb49c91ba593a9b1bcd8246bce82c
Hồ Lê Minh Thạch <thach.holeminh@gmail.com>
2022-05-30 19:29:04 +0700

    Initial commit

---
+++ controller/users.js
@@ -0,0 +1,120 @@
+import argon2 from 'argon2';
+import { User } from '../model/User.js';
+
+export const getUsers = async (req, res) => {
+	try {
+		const users = await User.find();
+		res.status(200).json(users);
+	} catch (err) {
+		res.status(500).json({ err: err });
+	}
+};
+
+export const register = async (req, res) => {
+	const { username, email, password } = req.body;
+	if (!username || !email || !password) {
+		return res.status(200).json({
+			success: false,
+			message: 'Missing username/email/password',
+		});
+	}
+
+	try {
+		let user = await User.findOne({ email });
+		if (user) {
+			return res
+				.status(200)
+				.json({ success: false, message: 'Email already registered' });
+		}
+		user = await User.findOne({ username });
+		if (user) {
+			return res.status(200).json({
+				success: false,
+				message: 'Username already registered',
+			});
+		}
+
+		const hashedPassword = await argon2.hash(password);
+		const newUser = new User({ username, email, password: hashedPassword });
+
+		await newUser.save();
+		res.status(200).json({
+			success: true,
+			message: 'User saved successfully',
+		});
+	} catch (err) {
+		console.log(err);
+		res.status(500).json({
+			success: false,
+			message: 'Internal server error',
+		});
+	}
+};
+
+export const login = async (req, res) => {
+	const { email, password } = req.body;
+	if (!email || !password) {
+		return res.status(200).json({
+			response: {
+				success: false,
+				message: 'Missing email/password',
+			},
+		});
+	}
+
+	try {
+		const user = await User.findOne({ email });
+		if (!user) {
+			return res.status(200).json({
+				response: {
+					success: false,
+					message: 'Incorrect email or password',
+				},
+			});
+		}
+
+		const passwordValid = await argon2.verify(user.password, password);
+		if (!passwordValid) {
+			return res.status(200).json({
+				response: {
+					success: false,
+					message: 'Incorrect email or password',
+				},
+			});
+		}
+
+		res.status(200).json({
+			response: {
+				success: true,
+				message: 'User login successfully',
+			},
+			data: {
+				username: user.username,
+				email: user.email,
+			},
+		});
+	} catch (err) {
+		console.log(err);
+		res.status(500).json({
+			response: {
+				success: false,
+				message: 'Internal server error',
+			},
+		});
+	}
+};
+
+// export const updateUser = async (req, res) => {
+// 	try {
+// 		const updateUser = req.body;
+
+// 		const user = await User.findOneAndUpdate(
+// 			{ _id: updateUser._id },
+// 			updateUser,
+// 			{ new: true }
+// 		);
+// 		res.status(200).json(user);
+// 	} catch (err) {
+// 		res.status(500).json({ err: err });
+// 	}
+// };
---
+++ index.js
@@ -0,0 +1,35 @@
+import express from 'express';
+import bodyParser from 'body-parser';
+import cors from 'cors';
+
+import users from './routers/users.js';
+import mongoose from 'mongoose';
+
+const app = express();
+const PORT = process.env.port || 5000;
+
+app.use(bodyParser.json({ limit: '30mb' }));
+app.use(bodyParser.urlencoded({ extended: true, limit: '30mb' }));
+app.use(cors());
+
+app.use('/users', users);
+
+app.get('/', (req, res) => {
+	res.send('SUCCESS');
+});
+
+const URI = `mongodb+srv://ThachHo:ThachDeptrai@movie-epam.czueohg.mongodb.net/movie-epam-v1?retryWrites=true&w=majority`;
+mongoose
+	.connect(URI, {
+		useNewUrlParser: true,
+		useUnifiedTopology: true,
+	})
+	.then(() => {
+		console.log('Connected to DB');
+		app.listen(PORT, () => {
+			console.log(`Server is running on port ${PORT}`);
+		});
+	})
+	.catch((err) => {
+		console.log('err', err);
+	});
